de7777efdbaf2e136d686ff6210ab844f33e9610
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-20 13:03:33 +0530

    Update Structures_theory.c

--- Structures_theory.c
+++ Structures_theory.c
@@ -1,1 +1,45 @@
+structures notes 
 
+In C programming, a structure is a user-defined data type that allows you 
+to combine different data types into a single entity. 
+It provides a way to group related data together and organize it under a single name. 
+Structures allow you to create complex data structures to represent real-world entities or concepts.
+
+The syntax for defining a structure in C is as follows:
+
+struct StructureName {
+    dataType1 member1;
+    dataType2 member2;
+    // ... more members
+};
+
+The struct keyword is used to define a structure,
+followed by the name of the structure (StructureName in the example above). 
+Inside the curly braces, you specify the members of the structure, which can be of any valid C data type.
+
+Once you have defined a structure, you can create variables of that structure type. 
+For example:
+
+struct StructureName variableName;
+
+You can then access the members of the structure using the dot (.) operator.
+For example:
+
+variableName.member1 = value1;
+variableName.member2 = value2;
+
+data structures.
+
+You can pass structures as function arguments and return them from functions. 
+When passing structures as function arguments, you can pass them by value or by reference 
+(using pointers). Passing structures by reference allows you to 
+modify the original structure within the function.
+
+Here are some key points to remember about structures in C:
+
+Structures are used to group related data together.
+They allow you to define custom data types.
+Structure members can have different data types.
+Structures can be nested within other structures.
+You can access structure members using the dot (.) operator.
+Structures can be passed as function arguments and returned from functions.
